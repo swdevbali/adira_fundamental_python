46c578731f9ec88cdfce61e3e6df805cd51b8fc2
Eko Wibowo <swdev.bali@gmail.com>
2019-10-29 11:48:26 +0700

    Dictionary

--- fundamental_01.py
+++ fundamental_01.py
@@ -7,6 +7,7 @@ Fundamental ada tiga:
 - Sequential
 - Branching
 - Loop / Perulangan
+- Tipe data penting: List (array) dan Dictionary (kamus/json)
 - Modularization:
     - Dengan Fungsi
     - Dengan Class
@@ -28,9 +29,18 @@ NON_FIX, FIX = range(2)
 
 customer1_nama = 'Budi'
 customer1_jenis_kelamin = PRIA
-customer1_gaji = 3000000
+customer1_gaji = 30000000
 customer1_existing = True
 customer1_occupation = FIX
+customer1_alamat = {
+    'Line 1': 'Glagah Kidul RT 3',
+    'Kelurahan': 'Tamanan',
+    'Kecamatan': 'Banguntapan',
+    'City': 'Bantul',
+    'Province': 'Yogyakarta',
+    'Zip Code': '55191',
+    'Country': 'Indonesia'
+}
 
 customer2_nama = 'Wati'
 customer2_jenis_kelamin = WANITA
@@ -48,9 +58,48 @@ print(f'{nama_program} versi {versi} oleh {author}')
 
 if customer1_existing:
     print(f'{customer1_nama} sedang memiliki pinjaman')
+else:
+    print(f'{customer1_nama} sedang tidak memiliki pinjaman')
 
 if customer2_existing:
     print(f'{customer2_nama} sedang memiliki pinjaman')
+else:
+    print(f'{customer2_nama} sedang tidak memiliki pinjaman')
 
 if customer3_existing:
     print(f'{customer3_nama} sedang memiliki pinjaman')
+else:
+    print(f'{customer3_nama} sedang tidak memiliki pinjaman')
+
+
+#kasus customer 1 jenjang gaji: <3jt, 3jt - 6jt, 6jt - 9jt
+print(f'Pengecekan gaji {customer1_nama} sebesar {customer1_gaji}')
+if customer1_gaji < 3000000 :
+    print('Terlalu kecil gajinya')
+elif customer1_gaji >= 3000000 and customer1_gaji < 6000000:
+    print('Kelas menengah')
+elif customer1_gaji >= 6000000 and customer1_gaji < 9000000:
+    print('Kelas atas')
+else:
+    print('Dewaaa')
+
+#COBA CETAK SEMUA NAMA CUSTOMER
+#tanpa list
+print(customer1_nama)
+print(customer2_nama)
+print(customer3_nama)
+
+#dengan list
+print()
+print('Daftar customers')
+customers = [customer1_nama, customer2_nama, customer3_nama]
+for i in range(0, len(customers)):
+    print(f'{i+1}. {customers[i]}')
+
+print(customer1_alamat)
+print(customer1_alamat['Line 1'])
+print(customer1_alamat['Kelurahan'])
+print(customer1_alamat['Kecamatan'])
+print(customer1_alamat['City'])
+print(customer1_alamat['Province'])
+print(customer1_alamat['Zip Code'])
